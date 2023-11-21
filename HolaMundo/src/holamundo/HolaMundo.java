9fd57746257a53f65e713d39150a598c6a74fc83
Fran <alcantarilla.cafra24@triana.salesianos.edu>
2023-11-21 14:05:52 +0100

    Se mejora los alumnos

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -4,9 +4,9 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("-".repeat(50));
 		System.out.println("Hola Mundo");
-		
+		System.out.println("-".repeat(50));
 	}
 
 }
